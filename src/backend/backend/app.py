300d3e0a04e833f80ad0b40f12682a700fad5b65
Giuseppe Ambrosio <giuambro97@gmail.com>
2024-10-30 12:35:36 +0100

    feat(be): scaffolding

---
+++ src/backend/backend/app.py
@@ -0,0 +1,21 @@
+import logging
+
+from fastapi import FastAPI
+
+from backend.routers import health
+from fastapi.middleware.cors import CORSMiddleware
+
+logger = logging.getLogger(__name__)
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(health.router)
